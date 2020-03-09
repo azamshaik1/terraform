52dad78ff9d4939649bf88dafe2b5e199d500a95
Azam Shaik <ashaik@ascap.com>
2020-03-09 14:23:12 -0400

    Initial Commit

---
+++ 02_getting_started/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-08bc77a2c7eb2b1da"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
